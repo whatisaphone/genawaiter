ff80a0f27c3f5a88d3d3de5e1459e5ca27cb6592
John Simon <john@whatisaph.one>
2019-11-02 19:03:35 -0400

    no panic

--- src/waker.rs
+++ src/waker.rs
@@ -7,11 +7,16 @@ pub fn create() -> Waker {
     unsafe { Waker::from_raw(RAW_WAKER) }
 }
 
-const VTABLE: RawWakerVTable = RawWakerVTable::new(
-    /* clone */ |_| panic!(),
-    /* wake */ |_| {},
-    /* wake_by_ref */ |_| {},
-    /* drop */ |_| {},
-);
-
 const RAW_WAKER: RawWaker = RawWaker::new(ptr::null(), &VTABLE);
+
+const VTABLE: RawWakerVTable = RawWakerVTable::new(clone, wake, wake_by_ref, drop);
+
+unsafe fn clone(_: *const ()) -> RawWaker {
+    RAW_WAKER
+}
+
+unsafe fn wake(_: *const ()) {}
+
+unsafe fn wake_by_ref(_: *const ()) {}
+
+unsafe fn drop(_: *const ()) {}
